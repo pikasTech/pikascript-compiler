5cba19c9f796086aeef47f86ec5a583112af7701
lyon <645275593@qq.com>
2021-08-08 18:00:20 +0800

    minimal api.py

--- src/mimiscript-api.py
+++ src/mimiscript-api.py
@@ -1,28 +1,19 @@
 class PyObj(TinyObj):
     def importClass(className: str, fp: FILE):
         pass
-
     def newObj(objName: str, className: str, fp: FILE):
         pass
-
-
 class PyClass (SysObj):
     def setSuper(superClassName: str):
         pass
-
     def makeApi(path: str):
         pass
-
     def makeHead(path: str):
         pass
-
-
 class PyMethod (SysObj):
     def makeMethodFun(fp: FILE):
         pass
-
     def makeMethodDeclear(fp: FILE):
         pass
-
     def makeMethodDefine(fp: FILE):
         pass
