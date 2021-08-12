77beeee91ac295a201497191e9bc125b21577da5
graduateDesign <graduateDesign@example.com>
2021-08-12 15:08:36 +0800

    move all mimitest to google test

---
+++ src/test/com-test.cpp
@@ -0,0 +1,26 @@
+#include "gtest/gtest.h"
+extern "C"
+{
+#include "mimiCom.h"
+#include "dataString.h"
+}
+
+TEST(com_test, test1)
+{
+    MimiCom *com = New_mimiCom(NULL);
+    com->getChar(com, 'a');
+    com->getChar(com, 'a');
+    com->getChar(com, 'a');
+    com->getChar(com, '\r');
+    com->getChar(com, '\n');
+
+    char *RxSingleLine = args_getStr(com->args, (char *)"RxSingleLine");
+    EXPECT_TRUE(strEqu((char *)"aaa", RxSingleLine));
+    com->deinit(com);
+}
+
+extern DMEM_STATE DMEMS;
+TEST(com_test, test_mem)
+{
+    EXPECT_EQ(DMEMS.blk_num, 0);
+}
