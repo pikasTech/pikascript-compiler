15ea429311bc43f9a5392bdadc5943527212b6d3
graduateDesign <graduateDesign@example.com>
2021-08-12 18:05:12 +0800

    add debug file

--- src/test/test-mimiscript-api5.py
+++ src/test/test-mimiscript-api5.py
@@ -1,2 +1,11 @@
 class Compiler(SysObj):
+    # import class only
+    PyMethod()
     PyClass()
+    PyObj()
+
+    def build(pythonApiPath: str, outputPath: str):
+        pass
+
+    def analizeFile(pythonApiPath: str):
+        pass
